d01a6b747951645bf22cbf39642847c383ff92a6
jamiejonescera <152496126+jamiejonescera@users.noreply.github.com>
2025-01-20 01:31:47 +0800

    Deploy 14

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/",
+  base: "/frontend/",
   plugins: [react()],
+  build: {
+    outDir: "build", // Change the output directory to 'build'
+  },
   server: {
     host: "0.0.0.0",
     port: 3002,
